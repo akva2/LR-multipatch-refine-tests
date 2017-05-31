01b2b08c6fda68cf3b1ebb524922ae274dc962f1
Kjetil Andre Johannessen <kjetijo@gmail.com>
2017-05-31 10:16:50 +0200

    Added Lshape test case, file reading input, and two more assertions

--- geometries/generate.py
+++ geometries/generate.py
@@ -24,6 +24,8 @@ s2 = s1 + [1,0]
 s3 = s2 - [0,1]
 s3.raise_order(0,1) # !!! Different polynomial order for this patch
 lshape = [s1,s2,s3]
+for surf in lshape:
+  surf.refine(5,5)
 with G2("lshape.g2") as f:
     f.write(lshape)
 
--- src/TestMatchingKnots.cpp
+++ src/TestMatchingKnots.cpp
@@ -3,12 +3,37 @@
 #include <cmath>
 #include <algorithm>
 #include "LRSpline/LRSplineSurface.h"
+#include "GoTools/geometry/SplineSurface.h"
+#include "GoTools/geometry/ObjectHeader.h"
 
 using namespace std;
 using namespace LR;
+using namespace Go;
 
+// read multipatch g2 file and return LRSplineSurface representation of all patches
+vector<LRSplineSurface*> readFile(const string &filename) {
+  vector<LRSplineSurface*> results;
+  ObjectHeader head;
+  SplineSurface ss;
 
-// check that new knots contain all original knots
+  ifstream in(filename);
+  if(!in.good()) {
+    cerr << "could not open file: " << filename << endl;
+    return results;
+  }
+
+  while(!in.eof()) { // for all patches
+    in >> head >> ss;
+    results.push_back(new LRSplineSurface(&ss));
+    ws(in); // eat whitespaces
+  }
+
+  for(auto lr : results)
+    lr->generateIDs();
+  return results;
+}
+
+// check that new knots contain all original knots (one-way matching)
 static bool check_orig_knots_is_subset(const std::vector<double>& knots, const std::vector<double>& new_knots) {
   for (auto& it : new_knots)
     if (std::find_if(knots.begin(), knots.end(), [it](double a){return fabs(it-a) < 1e-4;}) == knots.end())
@@ -17,6 +42,25 @@ static bool check_orig_knots_is_subset(const std::vector<double>& knots, const s
   return true;
 }
 
+// check that knots vectors are identical (two-way matching)
+static bool check_matching_knots(const std::vector<double>& knots1, const std::vector<double>& knots2) {
+  if(knots1.size() != knots2.size())
+    return false;
+  for(size_t i=0; i<knots1.size(); ++i)
+    if(fabs(knots1[i]-knots2[i]) > 1e-4)
+      return false;
+
+  return true;
+}
+
+// check that all elements continue to be perfect squares (isotropic / basis-function refinement)
+static bool check_isotropic_refinement(LRSplineSurface *lr) {
+  for(auto el : lr->getAllElements()) 
+    if(fabs( (el->umax() - el->umin()) - (el->vmax() - el->vmin()) ) >1e-4)
+      return false;
+
+  return true;
+}
 
 // 2 patch, flip + reverse
 static int case1() {
@@ -56,6 +100,41 @@ static int case1() {
   return 0;
 }
 
+// 3 patch, Lshape
+static int case2() {
+  vector<LRSplineSurface*> lr          = readFile("../geometries/lshape.g2");
+  vector<Basisfunction*>   oneFunction ;
+  lr[0]->getEdgeFunctions( oneFunction, SOUTH_EAST );
+
+  // do a corner refinement at the kink of the L-shape (patch 0)
+  lr[0]->refineBasisFunction( oneFunction[0]->getId() );
+
+  // match patch 0 to patch 1
+  vector<double> knots1 = lr[0]->getEdgeKnots(EAST, true);
+  vector<double> knots2 = lr[1]->getEdgeKnots(WEST, true);
+  lr[0]->matchParametricEdge(EAST, knots2, true);
+  lr[1]->matchParametricEdge(WEST, knots1, true);
+
+  // match patch 1 to patch 2
+  vector<double> knots3 = lr[1]->getEdgeKnots(SOUTH, true);
+  vector<double> knots4 = lr[2]->getEdgeKnots(NORTH, true);
+  lr[1]->matchParametricEdge(SOUTH, knots4, true);
+  lr[2]->matchParametricEdge(NORTH, knots3, true);
+
+  // check that it all worked well
+  if(! check_matching_knots(lr[0]->getEdgeKnots(EAST, true),
+                            lr[1]->getEdgeKnots(WEST, true)))
+    return 1;                           
+  if(! check_matching_knots(lr[1]->getEdgeKnots(SOUTH, true),
+                            lr[2]->getEdgeKnots(NORTH, true)))
+    return 1;                           
+  for(auto l : lr)
+    if(! check_isotropic_refinement(l) )
+      return 1;
+
+  return 0;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cerr << "Need a parameter - the test to run" << std::endl;
@@ -65,6 +144,8 @@ int main(int argc, char **argv) {
 
   if (atoi(argv[1]) == 1)
     return case1();
+  else if (atoi(argv[1]) == 2)
+    return case2();
 
   return 1;
 } 
